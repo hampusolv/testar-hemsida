2f340dcbc44f5e50bb1b80613cca28f5d3588b33
hanino87 <148004074+hanino87@users.noreply.github.com>
2024-08-28 16:22:47 +0200

    added more test to my branch file for test

---
+++ test/hannes.spec.js
@@ -0,0 +1,11 @@
+;
+
+import { test, expect } from '@playwright/test';
+
+test('test1', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/hemsida_material/contact.html');
+  await page.getByPlaceholder('Name').click();
+  await expect(page.getByPlaceholder('Name')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'SEND' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'SEND' })).not.toBeHidden();
+});
